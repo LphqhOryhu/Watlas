dfb63d8682bd71934aa631ecbd1c07aae6a8063f
DE BRABANDER Hugo <hdb@sga-industries.com>
2025-07-03 15:30:46 +0200

    fix for deploy

--- watlas/pages/api/add-page.ts
+++ watlas/pages/api/add-page.ts
@@ -16,7 +16,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
 
         res.status(200).json({ success: true, data });
-    } catch (_error) {
+    } catch {
         res.status(500).json({ error: 'Erreur serveur' });
     }
 }
